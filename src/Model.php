1cfcae3f6ca170b07d7081d7bf794768d6423595
David Plath <support@krealmwebservices.com.au>
2020-03-04 22:47:16 +1000

    Added base classes for creating controllers and models

---
+++ src/Model.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     Telcoworks Group PHP Library
+ * @copyright   Copyright (c) 2020 Telcoworks Group. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @author      David Plath <webmaster@telcoworksgroup.com.au>
+ * =============================================================================
+ */
+
+namespace TelcoworksGrp\Legacy;
+
+
+/**
+ * Base class for creating models
+ */
+class Model
+{
+}
